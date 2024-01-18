927374f37ca4490524673368200feef411e8d579
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2024-01-18 22:12:09 +0000

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
-"""Module for 0-minoperations"""
+'''The minimum operations coding challenge.
+'''
 
 
 def minOperations(n):
-    """Calculates the fewest number of operations needed to result
-    in exactly n 'H' characters in the file."""
-    if n < 2:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-    operations = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
-            operations += i
-            n //= i
-    return operations
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
